21da26a4d68ec8bfa71613295460b25f360c2694
Karan Arora <arora.2003.karan@gmail.com>
2025-01-20 23:34:03 +0530

    last

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<BirthdayGreeting />} />
+        <Route path="/happy-birthday" element={<BirthdayGreeting />} />
         <Route path="/surprise" element={<Surprise />} />
         <Route path="/message" element={<Message />} />
       </Routes>
